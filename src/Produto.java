338c2b1c1cd31d070a930f7ac7ed97e95e4fa3f6
gabriellananda <gabriella-nanda@hotmail.com>
2026-03-02 22:29:50 -0300

    refatora classe Produto

--- src/Produto.java
+++ src/Produto.java
@@ -1,9 +1,11 @@
 import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class Produto {
+public abstract class Produto {
 	
 	private static final double MARGEM_PADRAO = 0.2;
-	private String descricao;
+	protected String descricao;
 	protected double precoCusto;
 	protected double margemLucro;
 	
@@ -62,9 +64,58 @@ public class Produto {
      */
     @Override
 	public String toString() {
-    	
     	NumberFormat moeda = NumberFormat.getCurrencyInstance();
-    	
 		return String.format("NOME: " + descricao + ": " + moeda.format(valorVenda()));
 	}
+
+	/**
+	* Igualdade de produtos: caso possuam o mesmo nome/descrição.
+	* @param obj Outro produto a ser comparado
+	* @return booleano true/false conforme o parâmetro possua a descrição igual ou não a este produto.
+	*/
+	@Override
+	public boolean equals(Object obj){
+		Produto outro = (Produto)obj;
+		return this.descricao.toLowerCase().equals(outro.descricao.toLowerCase());
+	}
+
+	/**
+	* Gera uma linha de texto a partir dos dados do produto
+	* @return Uma string no formato "tipo; descrição;preçoDeCusto;margemDeLucro;[dataDeValidade]"
+	*/
+	public abstract String gerarDadosTexto();
+
+	/**
+	* Cria um produto a partir de uma linha de dados em formato texto. A linha de dados deve estar de acordo com a
+	formatação
+	* "tipo; descrição;preçoDeCusto;margemDeLucro;[dataDeValidade]"
+	* ou o funcionamento não será garantido. Os tipos são 1 para produto não perecível e 2 para perecível.
+	* @param linha Linha com os dados do produto a ser criado.
+	* @return Um produto com os dados recebidos
+	*/
+	static Produto criarDoTexto(String linha){
+		Produto novoProduto = null;
+		
+		/*Você deve implementar aqui a lógica que separa os dados existentes na String linha, verifica se o produto é do
+		tipo 1 ou 2 e constrói o objeto adequado, com os dados fornecidos de acordo com seu tipo. O objeto construído é
+		retornado pelo método*/
+
+		DateTimeFormatter formatoData = DateTimeFormatter.ofPattern(("dd/MM/yyyy"));
+		String[] atributos = linha.split(";");
+		int tipo = Integer.parseInt(atributos[0]);
+		String descricao = atributos[1];
+		double preco = Double.parseDouble(atributos[2]);
+		double margem = Double.parseDouble(atributos[3]);
+		
+		if(tipo == 1) {
+			novoProduto = new ProdutoNaoPerecivel(descricao, preco, margem);
+		}
+		else {
+			LocalDate dataValidade = LocalDate.parse(atributos[4], formatoData);
+			novoProduto = new ProdutoPerecivel(descricao, preco, margem, dataValidade);
+		}
+		return novoProduto;
+	}
+
+
 }
